1cc42d516d62e2ab890a843feeeca68a039fa275
TimaCodingSchool <143556630+TimaCodingSchool@users.noreply.github.com>
2023-09-11 11:58:06 -0700

    Create squares.js

---
+++ squares.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(5);
+  fill(200)
+  square(50,50,300)
+  fill(150)
+  strokeWeight(10)
+  square(80,80,240)
+  fill(100)
+  strokeWeight(15)
+  square (110,110,170)
+  fill(75)
+  strokeWeight(20)
+  square(150,150,90)
+  strokeWeight(30)
+  fill(0)
+  square(195,195,1)
+  
+}
