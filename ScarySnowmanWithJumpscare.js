498569f5b3944aa010a322280fd8b975bb6dac6e
TimaCodingSchool <143556630+TimaCodingSchool@users.noreply.github.com>
2023-10-30 11:08:51 -0700

    Update ScarySnowmanWithJumpscare.js

--- ScarySnowmanWithJumpscare.js
+++ ScarySnowmanWithJumpscare.js
@@ -1,67 +1,61 @@
- let carrot;
+let carrot;
 let carrotBumps = [];
 
-let jumpscareTime = 10000; // 15 seconds in milliseconds
-let lastJumpscareTime = 0;
+let jumpscareTime = 15000; // 15 seconds in milliseconds
+let lastJumpscareTime = 2000;
 let jumpscareTriggered = false;
+let mouthArc; 
 
 function setup() {
   createCanvas(600, 600, WEBGL);
-  carrot = createVector(0, 0, 0);
-  for (let i = 0; i < 100; i++) {
+  
     let x = random(-50, 50);
     let y = random(-50, 50);
     let z = random(-50, 50);
-    carrotBumps.push(createVector(x, y, z));
-  }
+  
+  mouthArc = PI; 
 }
 
 function draw() {
-  background(0);
-  pointLight(200, 0, 0, 600, 600, 600);
-  pointLight(255, 255, 255, 0, 300, 100);
-
-  // Snowman
   if (!jumpscareTriggered) {
-    // Bottom sphere (snowman base)
+    background(0);
+    pointLight(200, 0, 0, 600, 600, 600);
+    pointLight(255, 255, 255, 0, 300, 100);
+
+    // Snowman
     push();
     translate(0, 50);
     sphere(100);
     pop();
 
-    // Middle sphere
     push();
     translate(0, -50);
     sphere(70);
     pop();
 
-    // Head sphere
     push();
     translate(0, -130, 0);
     sphere(50);
     pop();
 
-    // Hat cylinder
     push();
     fill(100, 0, 0);
     translate(0, -180, 0);
     cylinder(60, 10);
     pop();
 
-    // Top part of hat
     push();
     fill(100, 0, 0);
     translate(0, -190, 0);
     cylinder(40, 20);
     pop();
 
-    // Eyes spheres   
     push();
     translate(-15, -130, 100);
     fill(0);
     sphere(10, 10);
     pop();
-  
+
     push();
     translate(15, -130, 100);
     fill(0);
@@ -78,25 +72,33 @@ function draw() {
 
 function jumpscare() {
   jumpscareTriggered = true;
-  background(255);
+  background(225, 0, 0);
 
-  // Carrot
-  push();
-  translate(0, 120);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  // Carrot body
-  fill(255, 165, 79); // Orange color
-  cone(50, 200, 40, 10);
+  // Creepy smiley face
+  fill(255);
+  ellipse(0, 0, 200, 200); // face
 
-  // Carrot bumps
-  for (let i = 0; i < carrotBumps.length; i++) {
-    let b = carrotBumps[i];
-    push();
-    translate(b.x, b.y - 100, b.z);
-    sphere(5);
-    pop();
+  // Eyes
+  fill(0);
+  ellipse(40, 0, 20, 20); // left eye
+  ellipse(-40, 0, 20, 20); // right eye
+
+  // Mouth
+  noFill();
+  stroke(0);
+  strokeWeight(4);
+  arc(0, 40, 100, 60, 0, mouthArc); // mouth
+}
+
+function mouseClicked() {
+  if (jumpscareTriggered) {
+    // Check if the mouse is clicked within the smiley face's area
+    let d = dist(mouseX, mouseY, width / 2, height / 2);
+    if (d < 100) {
+      // Decrease the arc value
+      if (mouthArc > 0) {
+        mouthArc -= 0.1;
+      }
+    }
   }
-  pop();
-
 }
